b20b835460fa81d112208b19c484127d6d09974c
Raj Randive <randiveraj96@gmail.com>
2024-03-01 23:07:11 +0530

    worked with controllers and routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import cookieParser from "cookie-parser";
-import cors from "cors;";
+import cors from "cors";
 import express from "express";
 
 const app = express();
@@ -15,5 +15,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
+// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v1/users/login
 
 export { app };
